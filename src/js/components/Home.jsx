981941209e666314f01e821abb19ef4e0143fe6d
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-07-13 13:40:01 +0000

    falta estilo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,11 +5,15 @@ const Home = () => {
 
 	const [inputValue, setInputValue] = useState("")
 	const [tareas, setTareas] = useState([])
-
+	const borrarTarea = (indexEliminar) => {
+		const nuevasTareas = tareas.filter((tarea, index) => index !== indexEliminar);
+		setTareas(nuevasTareas);
+	};
 
 
 	return (
-		<div className="text-center">
+		<div className=" container-master text-center">
+			<h1 className="titulo-tareas">TAREAS</h1>
 
 			<input type="text"
 				value={inputValue}
@@ -23,16 +27,18 @@ const Home = () => {
 				}
 			/>
 
+			{tareas.length === 0 && <p>No hay tareas, añadir tareas</p>}
 			<ul>
 				{
 					tareas.map((tarea, index) => {
 						return (
-							<li key={index}> {tarea} </li>
+							<li key={index}> {tarea} <span onClick={() => borrarTarea(index)}> ❌</span> </li>
 						)
 					})
 				}
 
 			</ul>
+			<p>{tareas.length} tareas pendientes</p>
 
 		</div>
 	);
